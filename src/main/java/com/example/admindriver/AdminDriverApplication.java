6bb2ab988c8e560c5ed8f5de41bac3b6901c5d17
jfcardona05 <cardonareyj@gmail.com>
2026-04-13 10:37:37 -0500

    Primer commit proyecto Spring Boot

---
+++ src/main/java/com/example/admindriver/AdminDriverApplication.java
@@ -0,0 +1,13 @@
+package com.example.admindriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDriverApplication.class, args);
+    }
+
+}
